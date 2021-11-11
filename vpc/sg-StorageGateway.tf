68a1bddae2ea51bbc87588dd7714edd66a3e0018
donghyeok <sia3246@gmail.com>
2021-11-11 17:36:30 +0900

    sg-Storage

---
+++ vpc/sg-StorageGateway.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "terra-sg-StorageGateway"{
+    name = "terra-sg-StorageGateway"
+    description = "terra-sg-StorageGateway"
+    vpc_id = aws_vpc.terra-vpc.id
+    
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 445
+    to_port = 445
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+      Name = "terra-sg-StorageGateway"
+  }
+}
